e9bba106baf1a604c2facc0d2883bc492f8ed79d
Alae Bouhmala <41632039+alaebouhmaladev@users.noreply.github.com>
2022-03-24 16:49:37 +0100

    not complet FormResult.vue

--- src/components/FormResult.vue
+++ src/components/FormResult.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="form-root">
-        result
+        <div class="result-box">
+            <p class="result-title">Here you Advice</p>
+            <p class="result-text">your quotes is here</p>
+        </div>
     </div>
 </template>
 
@@ -11,11 +14,18 @@ export default {
 </script>
 
 <style scoped>
- .form-root{
+ .result-box{
     margin: 12px 0;
     padding: 15px;
     border-radius: 12px;
     color: aliceblue;
     background: #0a0a77;
+ }
+ .result-title{
+     font-size: 24px;
+     font-weight: bold;
+ }
+ .result-text{
+    font-size: 18px;
  }
 </style>
